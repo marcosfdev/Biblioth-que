fbaf1de296b00b82f1d9af0146d184f77ee9a6b6
Marc O'Brien <marc@marcobrien.co>
2020-05-06 18:08:24 -0700

    updating gqlgen/resolver.go

--- gqlgen/resolvers.go
+++ gqlgen/resolvers.go
@@ -11,3 +11,8 @@ type agentResolver struct{ *Resolver }
 func (r *agentResolver) Authors(ctx context.Context, obj *pg.Agent) ([]pg.Author, error) {
 	panic("not implemented")
 }
+
+// Resolver connects individual resolvers with the datalayer.
+type Resolver struct {
+	Repository pg.Repository
+}
